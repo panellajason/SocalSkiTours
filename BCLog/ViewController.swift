6fe919d4b2fd0ac500e23f8f646980a7ea6565ad
Jason Panella <panellajason@gmail.com>
2020-05-26 11:03:08 -0700

    Initial Commit

---
+++ BCLog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BCLog
+//
+//  Created by Jason Panella on 5/26/20.
+//  Copyright © 2020 Jason Panella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
